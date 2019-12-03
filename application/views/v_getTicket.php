61b661b7b1de24900fb069eb14fa6fd79bd7ad14
ariefhdt <blackconnor78@gmail.com>
2019-12-03 09:44:36 +0700

    finish how to get ticket page

--- application/views/v_getTicket.php
+++ application/views/v_getTicket.php
@@ -1,6 +1,11 @@
 <?php $this->load->view("_partials/header")?>
-<div style="margin: 15%; text-align: center">
-	<h2>On Process Building...!!!</h2>
-</div>
-<?php echo var_dump($getTicket)?>
+
+<div class="text-center" style="margin-top: 5%;"><h3><strong><i><?php echo $getTicket->title_blog ?></i></strong></h3></div>
+
+<section>
+	<div class="container">
+		<div class="text-justify"><p><?php echo $getTicket->content_blog ?></p></div>
+	</div>	
+</section>
+
 <?php $this->load->view("_partials/footer")?>
